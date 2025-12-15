20f46eb2ff0d3007bfcdd21651deb0aaa59eb0f8
fourthk <nrkumaa@gmail.com>
2025-12-15 13:43:01 +0700

    Update integration

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -126,7 +126,7 @@ const Login = () => {
 
 <div className="text-center mb-6">
   <img
-    src="/assets/logo-sakti.png"
+    src="/src/assets/logo-sakti.png"
     alt="SAKTI Logo"
     className="h-20 mx-auto drop-shadow-lg"
   />
